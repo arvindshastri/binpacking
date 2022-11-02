cd95ca90e759253a8b1a2da55a9ed3a3ae0b82f0
Arvind Shastri <arvindshastri64@gmail.com>
2022-11-02 09:52:46 -0400

    Refined oracle.xlsx comparison and Jburkardt read

--- sample_bench.py
+++ sample_bench.py
@@ -3,7 +3,7 @@ import inspect
 import pyperf
 from os import listdir
 from os.path import isfile, join, basename
-from macpacking.reader import DatasetReader, BinppReader
+from macpacking.reader import DatasetReader, BinppReader, JburkardtReader
 from macpacking.model import Online
 from macpacking.algorithms.online import \
     FirstFit, BestFit, WorstFit
@@ -11,28 +11,52 @@ from macpacking.algorithms.offline import \
     FirstFitDecreasing, BestFitDecreasing, WorstFitDecreasing, BenMaier
 
 
-CASES = './_datasets/binpp/N1C1W1'
-
 '''Credit: https://stackoverflow.com/questions/1796180/how-can-i-get-a-list-of-all-classes-within-current-module-in-python'''  # noqa: E501
 onlineClasses = [name for name, obj in inspect.getmembers(sys.modules['macpacking.algorithms.online'], inspect.isclass) if obj.__module__ == 'macpacking.algorithms.online']  # noqa: E501
 
 
 def main():
-    cases = list_case_files(CASES)
+    
+    # binppCases = [
+    #     './_datasets/binpp/N1C1W1/N1C1W1_A.BPP.txt',
+    #     './_datasets/binpp/N1C3W4/N1C3W4_A.BPP.txt',
+    #     './_datasets/binpp/N2C1W1/N2C1W1_A.BPP.txt',
+    #     './_datasets/binpp/N2C3W4/N2C3W4_A.BPP.txt',
+    #     './_datasets/binpp/N3C1W1/N3C1W1_A.BPP.txt',
+    #     './_datasets/binpp/N3C3W4/N3C3W4_A.BPP.txt',
+    #     './_datasets/binpp/N4C1W1/N4C1W1_A.BPP.txt',
+    #     './_datasets/binpp/N4C3W4/N4C3W4_A.BPP.txt'
+    # ]
+
+    # binppHardCases = './_datasets/binpp-hard'
+    # jburkardtCases = './_datasets/jburkardt'
+    # listOfBinppHardCases = list_case_files(binppHardCases)
+    # listOfJburkardtCases = list_case_files(jburkardtCases)
 
     algorithms = [BenMaier(), FirstFit(), BestFit(), WorstFit(), FirstFitDecreasing(), BestFitDecreasing(), WorstFitDecreasing()]  # noqa: E501
+
+    # runner = pyperf.Runner()
+    # execTime(binppCases, BinppReader, algorithms, runner)
+    # execTime(listOfBinppHardCases, BinppReader, algorithms, runner)
+    # execTime(listOfJburkardtCases, JburkardtReader, algorithms, runner)
+
+    ######
+
+    CASES = './_datasets/binpp/N1C1W1'
+    cases = list_case_files(CASES)
     
     print("Analysis of Average Bin Usage as Percentage")
     print("─" * 25)
-    analyzePercentage(cases, BinppReader, algorithms)
+    resultList = analyzePercentage(cases, BinppReader, algorithms)
+    analyzeOutput(resultList)
 
-    print("Analysis of Average Remaining Space")
-    print("─" * 25)
-    spaceBench(cases, BinppReader, algorithms)
+    # print("Analysis of Average Remaining Space")
+    # print("─" * 25)
+    # spaceBench(cases, BinppReader, algorithms)
 
-    print("Analysis of Average Bins")
-    print("─" * 25)
-    numberBins(cases, BinppReader, algorithms)
+    # print("Analysis of Average Bins")
+    # print("─" * 25)
+    # numberBins(cases, BinppReader, algorithms)
 
 
 # HELPER FUNCTIONS
@@ -46,6 +70,7 @@ def average(list, decimal):
 
 def className(algorithm):
     return type(algorithm).__name__
+
 
 def printOutput(list):
     print(*list.keys(), sep="\t")
@@ -65,6 +90,9 @@ def readOnline(cases, reader, algorithm):
     solutionList = []
     capacity = 0
     for case in cases:
+        lastLetter = case.replace(".txt", "")[-1]
+        if reader == JburkardtReader and lastLetter != 'c':
+            continue
         data = reader(case).online()
         capacity = data[0]
         solution = algorithm(data)
@@ -77,6 +105,9 @@ def readOffline(cases, reader, algorithm):
     solutionList = []
     capacity = 0
     for case in cases:
+        lastLetter = case.replace(".txt", "")[-1]
+        if reader == JburkardtReader and lastLetter != 'c':
+            continue
         data = reader(case).offline()
         capacity = data[0]
         solution = algorithm(data)
@@ -85,13 +116,23 @@ def readOffline(cases, reader, algorithm):
     return (solutionList, capacity)
 
 
-# RUNTIME BENCHMARKS
-def execTimeBench(cases: list[str], reader: DatasetReader, algorithm: Online):  # noqa: E501
-    runner = pyperf.Runner()
-    for case in cases:
-        name = basename(case)
-        data = reader(case).online()
-        runner.bench_func(name, algorithm, data)
+# RUNTIME BENCHMARK
+def execTime(cases: list[str], reader: DatasetReader, algorithms: list, runner):  # noqa: E501
+
+    for algorithm in algorithms:
+        for case in cases:
+            lastLetter = case.replace(".txt", "")[-1]
+            if reader == JburkardtReader and lastLetter != 'c':
+                continue
+
+            name = basename(case)
+
+            if algorithm in onlineClasses:
+                data = reader(case).online()
+            else:
+                data = reader(case).offline()
+                
+            runner.bench_func(name, algorithm, data)
 
 
 # ANALYZE PERCENTAGE USAGE
@@ -119,8 +160,6 @@ def analyzePercentage(cases: list[str], reader: DatasetReader, algorithms: list)
             averageUsage.append(avg)
 
         resultList[className(algorithm)] = averageUsage
-
-    analyzeOutput(resultList)
 
     return resultList
 
@@ -174,7 +213,7 @@ def numberBins(cases: list[str], reader: DatasetReader, algorithms: list):
         resultList[className(algorithm)] = averageBins
 
     analyzeOutput(resultList)
-    
+
     return resultList
 
 
