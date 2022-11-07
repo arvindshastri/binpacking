d0bdc811b5df4475c705dd4142bef33e8294b16a
Stanley Nguyen <nguyes44@mcmaster.ca>
2022-11-06 23:35:11 -0500

    test cases for algos and jburkardt

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,4 +1,4 @@
-from macpacking.reader import DatasetReader, BinppReader
+from macpacking.reader import DatasetReader, BinppReader, JburkardtReader
 
 
 def test_binpp_reader():
@@ -10,5 +10,18 @@ def test_binpp_reader():
         72, 75, 76, 76, 79, 83, 83, 88, 92, 92, 93, 93, 97, 97, 97, 99, 100
     ]
     reader: DatasetReader = BinppReader(dataset)
+    assert capacity == reader.offline()[0]
+    assert oracle == sorted(reader.offline()[1])
+
+def test_jburkardt_reader():
+    dataset = '_datasets/binpp/N1C1W2/N1C1W2_A.BPP.txt'
+    capacity = 50
+    oracle = [
+        96, 93, 86, 86, 85, 83, 80, 80, 80, 79, 77, 68, 67,
+        64, 64, 63, 60, 57, 55, 54, 54, 54, 54, 52, 52, 52,
+        51, 44, 43, 41, 41, 39, 39, 39, 38, 36, 36, 35, 34, 
+        34, 31, 31, 29, 29, 28, 24, 23, 22,22, 20
+    ]
+    reader: DatasetReader = JburkardtReader(dataset)
     assert capacity == reader.offline()[0]
     assert oracle == sorted(reader.offline()[1])
